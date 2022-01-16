c251724bc7ff370984a8caf1b4ac8c9a86473f63
janzuiderveld <janzuiderveld@gmail.com>
2022-01-16 12:08:48 +0000

    January 16, 2022 12:08 PM

--- train.py
+++ train.py
@@ -58,12 +58,12 @@ parser.add_argument('--edge_weight', type=int, default= 5)
 # parser.add_argument('--sketch_embed_weight', type=int, default= 0)
 
 # parser.add_argument('--embedding_avg', type=str, default="/content/Sketch-Simulator/results/ovl_mean_sketch.pth")
-parser.add_argument('--embedding_avg', type=str, default="/content/Sketch-Simulator/results/ovl_mean_small.pth")
-# parser.add_argument('--embedding_avg', type=str, default="/content/drive/MyDrive/AI/sketch-to-image/overall_embeddings/ovl_mean_sketchy_vanilla.pt")
+# parser.add_argument('--embedding_avg', type=str, default="/content/Sketch-Simulator/results/ovl_mean_small.pth")
+parser.add_argument('--embedding_avg', type=str, default="/content/drive/MyDrive/AI/sketch-to-image/overall_embeddings/ovl_mean_sketchy_vanilla.pt")
 
-parser.add_argument('--target_avg_cuts', type=int, default=1)
+parser.add_argument('--target_avg_cuts', type=int, default=0)
 parser.add_argument('--target_det_cuts', type=int, default=0)
-parser.add_argument('--target_full_img', type=int, default=0)
+parser.add_argument('--target_full_img', type=int, default=1)
 parser.add_argument('--flavor', type=str, default="cumin", help='"ginger", "cumin", "holywater", "det"')
 
 # parser.add_argument('--reset_img_prompt_every', type=int, default= 0)
