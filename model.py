f3d46f5012cd2b019039e28c12593c4cc985b752
janzuiderveld <janzuiderveld@gmail.com>
2022-01-25 14:57:44 +0000

    January 25, 2022 2:57 PM

--- model.py
+++ model.py
@@ -194,8 +194,8 @@ class ModelHost:
 
     # IMAGE CONTENT PROMPT BIZZ ##########################$########
     if self.args.embedding_tgt:
-      embed = torch.load(self.args.embedding_tgt)
-      pMs.append(Prompt(embed, 1, -np.inf, name="tgt_embed").to(device))
+      embed = torch.load(self.args.embedding_tgt).unsqueeze(0)
+      pMs.append(Prompt(embed, 3, -np.inf, name="tgt_embed").to(device))
 
     path, weight, stop = parse_prompt(self.args.start_image)
     print("image weight", weight)
