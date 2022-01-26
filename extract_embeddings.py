12eb92ac6cdcecf3cc9251c73f4374b4100cabcd
janzuiderveld <janzuiderveld@gmail.com>
2022-01-26 20:35:35 +0000

    January 26, 2022 8:35 PM

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -18,52 +18,61 @@ from tqdm import tqdm
 
 def extract_sketch_embedding(paths):
     train.args.init_image = paths[0]
+    train.args.prompts = ""
     train.args.init_cutn = 32
+    # train.args.width = 
+    # train.args.height = 
+
     mh = ModelHost(train.args)
     os.makedirs(f"{args.save_root}/results", exist_ok=True)
 
-    # sample 20% of paths
-    paths = random.sample(paths, int(len(paths) * 0.2))
 
-    avg_embeddings = []
-    batch = []
-    for path in tqdm(paths):
-        init_img = mh.load_init_image(path, 400, 400, mh.device)
-        cuts = mh.make_cutouts_init(init_img)
-        cuts_norm = mh.normalize(cuts)
-        batch.append(cuts_norm)
+    for dataset_size in reversed([100000, 10000, 1000, 100]):
+        # sample 20% of paths
+        # paths = random.sample(paths, int(len(paths) * 0.2))
+        paths = random.sample(paths, dataset_size)
 
-        if len(batch) >= (5000 // mh.args.init_cutn):
+        avg_embeddings = []
+        batch = []
+        for path in tqdm(paths):
+            init_img = mh.load_init_image(path, 400, 400, mh.device)
+            cuts = mh.make_cutouts_init(init_img)
+            cuts_norm = mh.normalize(cuts.squeeze())
+            batch.append(cuts_norm)
+
+            if len(batch) >= (5000 // mh.args.init_cutn):
+                batch = torch.cat(batch, dim=0)
+                embed = mh.perceptor.encode_image(batch).float()
+                embed_avg = torch.mean(embed.double(), dim=0)
+                avg_embeddings.append((embed_avg.unsqueeze(0)))
+                batch = []
+                
+        elif batch:
             batch = torch.cat(batch, dim=0)
-            # input(batch.shape)
-           # batch = make_cutouts(TF.to_tensor(img).unsqueeze(0).to(device))
             embed = mh.perceptor.encode_image(batch).float()
-            # input(embed.shape)
             embed_avg = torch.mean(embed.double(), dim=0)
-            # input(embed_avg.shape)
             avg_embeddings.append((embed_avg.unsqueeze(0)))
             batch = []
-        
 
-    avg_embeddings = torch.cat(avg_embeddings, dim=0)
-    print(avg_embeddings.shape)
-    total_avg = torch.mean(avg_embeddings, dim=0)
-    print(total_avg.shape)
+            
 
-    torch.save(total_avg, f"{args.save_root}/results/ovl_mean_sketchy_photo_cutouts.pt")
+        avg_embeddings = torch.cat(avg_embeddings, dim=0)
+        print(avg_embeddings.shape)
+        total_avg = torch.mean(avg_embeddings, dim=0)
+        print(total_avg.shape)
+
+        torch.save(total_avg, f"{args.save_root}/results/ovl_mean_sketchy{dataset_size}_cutouts.pt")
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()    
     # parser.add_argument('--path', type=str, default = f"/content/Sketch-Simulator/256x256/sketch/**/**/*.png", help='image path(s).')
-    parser.add_argument('--path', type=str, default = f"/content/Sketch-Simulator/256x256/photo/**/**/*.jpg", help='image path(s).')
-    parser.add_argument('--items_per_class', type=int, default = 1000, help='Number of items to analyze per quickdraw class')
+    parser.add_argument('--path', type=str, default = f"/content/Sketch-Simulator/256x256/sketch/**/**/*.png", help='image path(s).')
     parser.add_argument('--save_root', type=str, default = "/content/Sketch-Simulator/", help='Root directory to save')
-    parser.add_argument('--padding', type=int, default = 0, help='If to pad images, if so which ratio to add on each side')
+    # parser.add_argument('--padding', type=int, default = 0, help='If to pad images, if so which ratio to add on each side')
     parser.add_argument('--width', type=int, default = 400)
     parser.add_argument('--height', type=int, default = 400)
-    parser.add_argument('--cutn', type=int, default = 64)
     args = parser.parse_args()
 
 
