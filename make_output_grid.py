96b82314e4e2607846b13461466201e2f885c309
janzuiderveld <janzuiderveld@gmail.com>
2022-01-26 18:11:07 +0000

    January 26, 2022 6:11 PM

--- make_output_grid.py
+++ make_output_grid.py
@@ -1,8 +1,27 @@
 
 import glob
-items = "/content/drive/MyDrive/AI/sketch-to-image/outputs/bullshit/*"
+items = "/content/drive/MyDrive/AI/sketch-to-image/outputs/bullshit/*.png"
 items = glob.glob(items)
 
+
+srcs = set()
+styles = set()
+
 for item in items:
+    print(item)
     src, style, num = item.split("_")
-    
+    srcs.add(src)
+    styles.add(style)
+
+import matplotlib.pyplot as plt
+for i, src in enumerate(srcs):
+    for j, style in enumerate(styles):
+        plt.subplot(len(srcs), len(styles), i*len(styles) + j + 1)
+        plt.imshow(plt.imread(glob.glob(f"/content/drive/MyDrive/AI/sketch-to-image/outputs/bullshit/{src}_{style}_*")[0]))
+        plt.axis('off')
+plt.show()
+plt.savefig("/content/drive/MyDrive/AI/sketch-to-image/outputs/bullshit/bullshit.png")
+
+
+
+# plot
