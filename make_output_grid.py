bc60599a1889e80e389c5b0f48e3dbed8728d0f5
janzuiderveld <janzuiderveld@gmail.com>
2022-01-26 18:14:45 +0000

    January 26, 2022 6:14 PM

--- make_output_grid.py
+++ make_output_grid.py
@@ -16,9 +16,16 @@ for item in items:
 import matplotlib.pyplot as plt
 for i, src in enumerate(srcs):
     for j, style in enumerate(styles):
-        plt.subplot(len(srcs), len(styles), i*len(styles) + j + 1)
-        plt.imshow(plt.imread(glob.glob(f"/content/drive/MyDrive/AI/sketch-to-image/outputs/bullshit/{src}_{style}_*")[0]))
-        plt.axis('off')
+        # fill a grid with images, style on x-axis, src on y-axis
+        plt.subplot(len(styles), len(srcs), i * len(styles) + j + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(plt.imread(f"/content/drive/MyDrive/AI/sketch-to-image/outputs/bullshit/{src}_{style}_0.png"))
+
+        # plt.subplot(len(srcs), len(styles), i*len(styles) + j + 1)
+        # plt.imshow(plt.imread(glob.glob(f"/content/drive/MyDrive/AI/sketch-to-image/outputs/bullshit/{src}_{style}_*")[0]))
+        # plt.axis('off')
 plt.show()
 plt.savefig("/content/drive/MyDrive/AI/sketch-to-image/outputs/bullshit/bullshit.png")
 
