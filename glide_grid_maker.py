e5b37eb84b82fb847907cc2bdae62713240c87f5
janzuiderveld <janzuiderveld@gmail.com>
2022-01-27 15:31:41 +0000

    January 27, 2022 3:31 PM

--- glide_grid_maker.py
+++ glide_grid_maker.py
@@ -9,6 +9,7 @@ scores = dict()
 
 for sel in selection:
     name, score = sel.split("/")[-1].split("_")
+    if name in ["spoon", "teapot",  "bicycle", "chair", "hamburger"]:continue
     print(score)
     score = score[:-4]
     print(score)
