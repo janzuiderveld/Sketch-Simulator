38370adbdc66762d5676217dd36f84693bec9e42
janzuiderveld <janzuiderveld@gmail.com>
2022-01-27 15:18:39 +0000

    January 27, 2022 3:18 PM

--- glide_grid_maker.py
+++ glide_grid_maker.py
@@ -25,9 +25,22 @@ top_10 = sorted(scores, key=scores.get, reverse=True)[:10]
 
 print(top_10)
 
+input_images = glob.glob("/content/drive/MyDrive/AI/sketch-to-image/a_complete_clean_and_recognizable_sketch/**")
+print(input_images)
+
+# make a 2 by 5 grid of the top 10
+img_size = Image.open(selection[0]).size
+
+
+
+
 for item in top_10:
     print(item)
     print(scores[item])
-#     img = Image.open(path_lookup[item])
-#     img = img.resize((256, 256))
-#     img.save(f"/content/drive/MyDrive/AI/sketch-to-image/glide_outputs_cut_selection/{item}.png")                               
+
+
+    img = Image.open(path_lookup[item])
+    img = img.resize((256, 256))
+
+
+    # img.save(f"/content/drive/MyDrive/AI/sketch-to-image/glide_outputs_cut_selection/{item}.png")                               
