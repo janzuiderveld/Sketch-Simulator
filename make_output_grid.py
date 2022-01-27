aaae3da3eb1b55a3d5f370299989789621458b73
janzuiderveld <janzuiderveld@gmail.com>
2022-01-27 09:59:50 +0000

    January 27, 2022 9:59 AM

--- make_output_grid.py
+++ make_output_grid.py
@@ -2,9 +2,10 @@ import glob
 from PIL import Image, ImageOps
 
 # folder_name = "dataset_size_experiment"
-folder_name = "non-selection"
+folder_name = "non_selection"
 items = f"/content/drive/MyDrive/AI/sketch-to-image/outputs/{folder_name}/*.png"
 items = glob.glob(items)
+
 
 # items_src = "/content/drive/MyDrive/AI/sketch-to-image/a_complete_clean_and_recognizable_sketch/selection2/"
 
