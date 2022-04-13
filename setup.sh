df43c68d223170b7344ace3c08bf95df372f559f
janzuiderveld <janzuiderveld@gmail.com>
2022-04-13 21:21:21 +0000

    up

--- setup.sh
+++ setup.sh
@@ -13,5 +13,5 @@ curl -L -o vqgan_imagenet_f16_16384.ckpt -C - 'https://heibox.uni-heidelberg.de/
 # curl -L -o wikiart_16384.ckpt -C - 'http://eaidata.bmk.sh/data/Wikiart_16384/wikiart_f16_16384_8145600.ckpt' 
 
 # Download sketchy dataset
-wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1z4--ToTXYb0-2cLuUWPYM5m7ST7Ob3Ck' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1z4--ToTXYb0-2cLuUWPYM5m7ST7Ob3Ck" -O rendered_256x256.7z && rm -rf /tmp/cookies.txt
-7z x rendered_256x256.7z
+# wget -nc --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1z4--ToTXYb0-2cLuUWPYM5m7ST7Ob3Ck' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1z4--ToTXYb0-2cLuUWPYM5m7ST7Ob3Ck" -O rendered_256x256.7z && rm -rf /tmp/cookies.txt
+# 7z x rendered_256x256.7z
