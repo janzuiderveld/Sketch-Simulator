77e2ad9ef114770ad9a64458503ae5220775bdea
Jan Zuiderveld <janzuiderveld@gmail.com>
2022-04-19 10:28:54 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 # install requirements.txt
+python3 -m pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
 python3 -m pip install -r requirements.txt 
 
 git clone https://github.com/CompVis/taming-transformers
